3ae065475f8c8fae22a0554a2eadd9afc000c393
Aedan Smith <aedan.smith@gmail.com>
2016-09-06 19:53:01 -0400

    Fixed Embedded Command Regex.

--- src/com/aedan/jterminal/commands/CommandHandler.java
+++ src/com/aedan/jterminal/commands/CommandHandler.java
@@ -13,6 +13,7 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.io.PrintStream;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Objects;
 import java.util.regex.Matcher;
 

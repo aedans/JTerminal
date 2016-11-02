9294acbb0c94674f272458fa24b42a17cf10cdc9
Aedan Smith <aedan.smith@gmail.com>
2016-11-02 09:31:51 -0700

    Improve JServerTerminal

--- src/com/aedan/jterminal/JServerTerminal.java
+++ src/com/aedan/jterminal/JServerTerminal.java
@@ -1,5 +1,6 @@
 package com.aedan.jterminal;
 
+import com.aedan.jterminal.environment.Environment;
 import com.aedan.jterminal.input.ScannerInput;
 import com.aedan.jterminal.output.CommandOutput;
 
@@ -22,25 +23,6 @@ public class JServerTerminal extends JTerminal {
     private Socket socket;
 
     /**
-     * Default JServerTerminal constructor.
-     *
-     * @throws Exception If there was an error hosting the JServerTerminal.
-     */
-    public JServerTerminal() throws Exception {
-        this(0);
-    }
-
-    /**
-     * Creates a JServerTerminal form a ServerSocket.
-     *
-     * @param port The port to host the JServerTerminal on.
-     * @throws Exception If there was an error hosting the JServerTerminal
-     */
-    public JServerTerminal(int port) throws Exception {
-        this(new ServerSocket(port));
-    }
-
-    /**
      * Creates a JServerTerminal from a ServerSocket.
      *
      * @param socket The ServerSocket to create the JServerTerminal from.
@@ -51,17 +33,39 @@ public class JServerTerminal extends JTerminal {
     }
 
     /**
+     * Creates a JServerTerminal from a ServerSocket.
+     *
+     * @param socket The ServerSocket to create the JServerTerminal from.
+     * @param environment The Environment for the JServerTerminal.
+     * @throws Exception If there was an error connecting to the socket.
+     */
+    public JServerTerminal(ServerSocket socket, Environment environment) throws Exception {
+        this(socket.accept(), environment);
+    }
+
+    /**
      * Creates a JServerTerminal from a Socket.
      *
      * @param socket The Socket to create the JServerTerminal from.
      * @throws Exception If there was an error connecting to the socket.
      */
     public JServerTerminal(Socket socket) throws Exception {
+        this(socket, null);
+    }
+
+    /**
+     * Creates a JServerTerminal from a Socket.
+     *
+     * @param socket The Socket to create the JServerTerminal from.
+     * @param environment The Environment for the JServerTerminal.
+     * @throws Exception If there was an error connecting to the socket.
+     */
+    public JServerTerminal(Socket socket, Environment environment) throws Exception {
         super(
                 "",
                 new ScannerInput(new Scanner(socket.getInputStream())),
                 new CommandOutput(new PrintStream(socket.getOutputStream())),
-                null
+                environment
         );
         this.socket = socket;
     }
